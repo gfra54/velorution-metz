49770be9d4060cce5147ca6cdf4a60d47f748314
alpha.sopress.dev <alpha@sopress.dev>
2021-09-24 10:31:20 +0200

    upd

--- wp-content/themes/velorution-metz/actions/api.php
+++ wp-content/themes/velorution-metz/actions/api.php
@@ -3,17 +3,24 @@ add_action('rest_api_init', function () {
     register_rest_route('velorution-metz', '/email/', array(
         'methods' => ['POST', 'GET'],
         'callback' => function () {
-
-            if ($email = $_POST['email']) {
+			$ret = false;
+			$email = $_POST['email'];
+			if(!$email) {
+				$email = $_GET['email'];
+			}
+            if ($email) {
                 if ($u = get_user_by('email', $email)) {
-                    return new WP_Error('user_exists', 'Il semble que vous soyez déjà inscrit avec cette adresse e-mail', array('status' => 409, 'email' => $email));
-                }
-                $password = random_readable_pwd();
-                if ($uid = wp_create_user($email, $password, $email)) {
-                    wp_new_user_notification( $uid);
-                    return get_user_by('id', $uid);
-                }
+                    $ret = new WP_Error('user_exists', 'Il semble que vous soyez déjà inscrit avec cette adresse e-mail', array('status' => 409, 'email' => $email));
+                } else {
+					$password = random_readable_pwd();
+					if ($uid = wp_create_user($email, $password, $email)) {
+						wp_new_user_notification( $uid);
+						$ret = get_user_by('id', $uid);
+					}
+				}
             }
+
+			return $ret;
         }
     ));
 });
