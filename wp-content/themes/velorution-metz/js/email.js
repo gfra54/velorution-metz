b7bd3e9042e0a37aba107ce3faa736482de978ab
Gilles FRANCOIS <gilles@sopress.net>
2021-09-24 10:23:02 +0200

    enqueue

--- wp-content/themes/velorution-metz/js/email.js
+++ wp-content/themes/velorution-metz/js/email.js
@@ -1,10 +1,10 @@
 //http://dev.local/velorution-metz/wp-json/velorution-metz/email
 
 $(document).ready(function () {
-    console.log('ok')
     $('form#email').submit(function (e) {
         let email = $(this).find('[type="email"]').val()
-        $.post('wp-json/velorution-metz/email', { email }).then(response => {
+
+        $.get('wp-json/velorution-metz/email?email='+encodeURIComponent(email)).then(response => {
             alert('Inscription réussie. Vous allez recevoir un e-mail de confirmation. A bientôt !');
             $(this).find('[type="email"]').val('')
         }).catch(error => {
